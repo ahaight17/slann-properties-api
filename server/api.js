b3dc5bb07e029959821ec6ea9549779d06b1396a
alex <alex.haight@gmail.com>
2022-01-13 15:02:21 -0600

    update api branding

--- server/api.js
+++ server/api.js
@@ -21,7 +21,7 @@ module.exports = (app) => {
   function index(req, res){
     res.status(200).send(
       `<p style="width: 100%; height: 100%; display: flex; justify-content: center; align-items: center; margin: 0">
-           A.H. API 
+           SLANNPROPERTIES.COM API  
            <br>VERSION: v0.0.1         
            <br>DATE: ${new Date().toLocaleDateString()} 
        </p>`
